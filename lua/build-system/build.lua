3dcf6f347e90f59bfd59290cbb50bf4c46eb45f9
Olle Lundmark <ollelundmark89@gmail.com>
2023-08-19 18:22:00 +0200

    Refactoring a bit

--- lua/build-system/build.lua
+++ lua/build-system/build.lua
@@ -1,32 +1,48 @@
 local path = require('plenary.path')
 local bs_file = require("build-system.file")
+local bs = require("build-system")
 local Dev = require("build-system.dev")
 local log = Dev.log
 
 local M = {}
 
-M.build_command = "make"
-
-local get_targets = function(command_file_content)
+local get_targets = function(command_file_content, command_regex)
     local commands = {}
-    for target in string.gmatch(command_file_content, "(%l-%S*):") do
+    for target in string.gmatch(command_file_content, command_regex) do
         table.insert(commands, target)
     end
     return commands
 end
 
+-- opts = {
+    -- build_file_type = "", -- default: bs.build_file_type (Makefile)
+    -- file_path = "", -- default: bs_file.find_build_file(opts)
+    -- command_regex = "", -- default: "(%l-%S*):"
+    -- get_commands_func = function(command_file_content, command_regex) -- default: get_targets
+-- }
 M.parse_command_file = function(opts)
     opts = opts or {}
 
-    local file_path = bs_file.find_build_file(opts)
-    if file_path == nil then
+    if opts.build_file_type == nil then
+        opts.build_file_type = bs.build_file_type
+    end
+
+    if opts.file_path == nil then
+        opts.file_path = bs_file.find_build_file(opts)
+    end
+
+    if opts.file_path == nil then
         print("Could not find build file.")
         return
     end
 
-    local file = io.open(file_path, "r")
+    if opts.command_regex == nil then
+        opts.command_regex = "(%l-%S*):"
+    end
+
+    local file = io.open(opts.file_path, "r")
     if file == nil then
-        print("Could not open file: " .. file_path)
+        print("Could not open file: " .. opts.file_path)
         return
     end
 
@@ -34,9 +50,9 @@ M.parse_command_file = function(opts)
 
     local commands = {}
     if opts.get_commands_func then
-        commands = opts.get_commands_func(command_file_content)
+        commands = opts.get_commands_func(command_file_content, opts.command_regex)
     else
-        commands = get_targets(command_file_content)
+        commands = get_targets(command_file_content, opts.command_regex)
     end
 
     return commands
@@ -49,22 +65,17 @@ M.create_output_buffer = function()
     vim.api.nvim_buf_set_option(bufnr, 'bufhidden', 'wipe')
     vim.api.nvim_buf_set_option(bufnr, 'swapfile', false)
 
-    return bufnr
-end
-
-M.use_output_buffer = function(bufnr)
     if vim.fn.bufexists(bufnr) == 0 then
         print("Buffer does not exist")
         return
     end
 
-    vim.cmd('belowright split')
-    -- TODO: make the size a option
-    vim.cmd('resize 15')
+    vim.cmd(string.format('%s split', bs.buffer_layout.split))
+    vim.cmd(string.format('resize %d', bs.buffer_layout.size))
+
     vim.api.nvim_win_set_buf(vim.api.nvim_get_current_win(), bufnr)
 
-    -- Delete the default empty line from the new buffer
-    vim.api.nvim_buf_set_lines(bufnr, 0, 1, false, {})
+    return bufnr
 end
 
 local set_cursor_to_end = function(bufnr)
@@ -90,6 +101,30 @@ M.print_to_buffer = function(bufnr, data)
     set_cursor_to_end(bufnr)
 end
 
+M.exit_print = function(exit_code, bufnr)
+    if vim.fn.bufexists(bufnr) == 0 then
+        return
+    end
+
+    if exit_code ~= 0 then
+        vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { "Job failed with exit code:" .. exit_code })
+    else
+        vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { "Job completed successfully!" })
+    end
+    set_cursor_to_end(bufnr)
+end
+
+-- opts = {
+    -- commands = {}, -- default: parsed commands from command file
+    -- build_command = "", -- default: bs.build_command (make)
+    -- ui_select = {
+        -- promt = "", -- default: "Choose what to build: "
+        -- kind = "", -- default: "build chooser"
+        -- on_format = function(result) -- default: just returns target
+        -- on_close = function() -- default: do nothing
+        -- on_user_choice = function(result) -- default: run build command via jobstart and print to buffer
+    -- } 
+-- }
 M.interactive_build = function(opts)
     opts = opts or {}
 
@@ -103,48 +138,35 @@ M.interactive_build = function(opts)
     end
 
     if not opts.build_command then
-        opts.build_command = M.build_command
+        opts.build_command = bs.build_command
     end
 
-    local bufnr = M.create_output_buffer()
+    opts.ui_select = opts.ui_select or {}
 
-    if not opts.user_choice_stdout then
-        opts.user_choice_stdout = function(_, data, _)
-            M.print_to_buffer(bufnr, data)
+    if opts.ui_select.promt == nil then
+        opts.ui_select.promt = "Choose what to build: "
+    end
+    if opts.ui_select.kind == nil then
+        opts.ui_select.kind = "build chooser"
+    end
+    if not opts.ui_select.on_format then
+        opts.ui_select.on_format = function(result)
+            return result
+        end
+    end
+    if not opts.ui_select.on_close then
+        opts.ui_select.on_close = function()
         end
     end
 
-    if not opts.user_choice_stderr then
-        opts.user_choice_stderr = function(_, data, _)
-            M.print_to_buffer(bufnr, data)
-        end
-    end
-
-    if not opts.user_choice_exit then
-        opts.user_choice_exit = function(_, exit_code, _)
-            if vim.fn.bufexists(bufnr) == 0 then
-                return
-            end
-
-            if exit_code ~= 0 then
-                vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { "Job failed with exit code:" .. exit_code })
-            else
-                vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { "Job completed successfully!" })
-            end
-            set_cursor_to_end(bufnr)
-        end
-    end
-
-    if not opts.working_directory then
+    if not opts.ui_select.on_user_choice then
         if bs_file.current_build_file == nil then
             print("No build file specified")
             return
         end
-        opts.working_directory = tostring(path:new(bs_file.current_build_file):parent())
-    end
+        local working_directory = tostring(path:new(bs_file.current_build_file):parent())
 
-    if not opts.on_user_choice then
-        opts.on_user_choice = function(result)
+        opts.ui_select.on_user_choice = function(result)
             if result == nil then
                 return
             end
@@ -153,40 +175,32 @@ M.interactive_build = function(opts)
             print(command_to_run)
 
             if opts.working_directory == nil then
-                if vim.fn.bufexists(bufnr) == 1 then
-                    vim.api.nvim_del_buf(bufnr, {})
-                end
                 print("No working directory specified")
                 return
             end
-            M.use_output_buffer(bufnr)
+
+            local bufnr = M.create_output_buffer()
             local _ = vim.fn.jobstart(command_to_run, {
-                cwd = opts.working_directory,
-                on_stdout = opts.user_choice_stdout,
-                on_stderr = opts.user_choice_stderr,
-                on_exit = opts.user_choice_exit,
+                cwd = working_directory,
+                on_stdout = function(_, data, _)
+                    M.print_to_buffer(bufnr, data)
+                end,
+                on_stderr = function(_, data, _)
+                    M.print_to_buffer(bufnr, data)
+                end,
+                on_exit = function(_, exit_code, _)
+                    M.exit_print(exit_code, bufnr)
+                end
             })
         end
     end
 
-
-    if not opts.ui_select_format then
-        opts.ui_select_format = function(result)
-            return result
-        end
-    end
-
-    if not opts.ui_on_close then
-        opts.ui_on_close = function()
-        end
-    end
-
     vim.ui.select(opts.commands, {
-        prompt = 'Choose what to build: ',
-        kind = 'build chooser',
-        on_format = opts.ui_select_format,
-        on_close = opts.ui_on_close,
-    }, opts.on_user_choice)
+        prompt = opts.ui_select.promt,
+        kind = opts.ui_select.kind,
+        on_format = opts.ui_select.on_format,
+        on_close = opts.ui_select.on_close,
+    }, opts.ui_select.on_user_choice)
 end
 
 return M
--- lua/build-system/init.lua
+++ lua/build-system/init.lua
@@ -6,13 +6,13 @@ Build_system_config = Build_system_config or {}
 local M = {}
 
 M.build_command = "make"
+M.build_file_type = "Makefile"
 
--- opts.source_file - string to the file you want sourced, should be relative to the project root
--- opts.source_file_function(opts, file_path) - a function that customizes what should be done with the source_file
--- opts.remove_commands - a table with the command names that should be removed, ex:
--- { "command_name1", "command_name2", ..., "command_name_X" }
--- opts.build_file_type - a string with the name of the build file being looked for, Default: "Makefile"
--- opts.build_command - a string with the name of the build command to be used, Default: "make"
+M.buffer_layout = {
+    size = 20,
+    split = "belowright"
+}
+
 M.setup = function(opts)
     opts = opts or {}
 
@@ -20,18 +20,13 @@ M.setup = function(opts)
         M.build_file_type = opts.build_file_type
     end
 
-    if opts.remove_commands then
-        M.remove_commands = opts.remove_commands
-    end
-
     if opts.build_command then
         M.build_command = opts.build_command
     end
 
-    if opts.interactive_build_function then
-        M.interactive_build_function = opts.interactive_build_function
+    if opts.buffer_layout then
+        M.buffer_layout = opts.buffer_layout
     end
 end
-
 
 return M
